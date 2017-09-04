ecd3fc273526d22b2caabe9c0f235821f6d9db98
0I0u3uk <svetlakovanton@gmail.com>
2017-09-04 13:57:25 +0700

    Refactoring example test

--- src/test/java/ru/sbtqa/apifactoryexample/entries/GetPostsEntry.java
+++ src/test/java/ru/sbtqa/apifactoryexample/entries/GetPostsEntry.java
@@ -5,23 +5,19 @@ import org.junit.Assert;
 import ru.sbtqa.tag.apifactory.ApiEntry;
 import ru.sbtqa.tag.apifactory.ApiFactory;
 import ru.sbtqa.tag.apifactory.annotation.ApiAction;
-import ru.sbtqa.tag.apifactory.annotation.ApiRequestParam;
 import ru.sbtqa.tag.apifactory.annotation.ApiValidationRule;
 import ru.sbtqa.tag.parsers.JsonParser;
 import ru.sbtqa.tag.parsers.core.exceptions.ParserException;
 
-@ApiAction(method = "get", title = "получить пост", path = "posts?id=%id")
-public class Post extends ApiEntry {
+@ApiAction(method = "get", title = "получить статьи", path = "posts")
+public class GetPostsEntry extends ApiEntry {
 
-    @ApiRequestParam(title = "id")
-    private static String id = "1";
-
-    @ApiValidationRule(title = "статус запроса успешен")
+    @ApiValidationRule(title = "верное количество статей")
     public void resultsCount() throws ParserException {
         String responce = ApiFactory.getApiFactory().getResponseRepository().getBody(this.getClass());
         JsonParser jparser = new JsonParser();
-        String userId = ((JSONArray)jparser.read(responce, "$.[:1].userId")).get(0).toString();
-        Assert.assertEquals("Incorrect userId", "1", userId);
+        JSONArray posts = jparser.read(responce, "$.[*]");
+        Assert.assertEquals("Incorrect count of posts", 100, posts.size());
 
     }
 }
